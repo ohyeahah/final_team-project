5b5eb35c16e80a4490c1621d218d9a81c7d2314c
ohyeahah <shirmpfairy@gmail.com>
2025-11-27 14:47:20 +0900

    OS 생성

---
+++ main.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+import tkinter as tk
+from tkinter import messagebox, font
+from PIL import Image, ImageTk
+import os
+import sys
+import tkinter.ttk as ttk
+import json
+from datetime import datetime
